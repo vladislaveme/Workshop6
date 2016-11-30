36dfe23bd5ca3b3d893b21049e5c2ddfbc864def
vladislaveme <vemelyanov@umass.edu>
2016-11-30 21:55:41 +0000

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,11 @@
-// Imports the express Node module.
 var express = require('express');
 // Creates an Express server.
 var app = express();
-// Defines what happens when it receives the `GET /` request
-app.get('/',function(req, res) {
-  res.send('Hello World!');
-});
 // Starts the server on port 3000!
 app.listen(3000,function() {
   console.log('Example app listening on port 3000!');
 });
-
-var reverseString = require('./util').reverseString;
+app.use(express.static('../client/build'));
 // Handle POST /reverse [data]
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
-app.post('/reverse',function(req, res) {
-  if(typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  }
-  else{
-    res.status(400).end();
-  }
-});
